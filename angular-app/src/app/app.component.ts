be4738a8c0dc60a82abcdeb7cac4be848686dd79
Eduardo Portella <eportella70@gmail.com>
2024-06-22 14:39:51 -0300

    first commit

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
+import { CustomersComponent } from './customers/customers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
-  styleUrl: './app.component.css',
-  template: `
-  <main>
-    <a [routerLink]="['/']">
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-    </a>
-    <section class="content">
-      <router-outlet></router-outlet>
-    </section>
-  </main>
-`,
+  imports: [CustomersComponent],
+  template: `<app-customers>`,
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'angular-app';
 }
